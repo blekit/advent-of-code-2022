0981d635de54d5b4706b050597c3464432645009
Michal Olejnik <michal.olejnik@ocado.com>
2022-12-04 10:07:12 +0100

    Advent of Code 2022 - Day 4: Camp cleanup...

---
+++ src/main/kotlin/com/adventofcode/day4/CampCleanupSolver.kt
@@ -0,0 +1,31 @@
+package com.adventofcode.day4
+
+import readInput
+
+fun main() {
+    println(solvePartOne())
+    println(solvePartTwo())
+}
+
+fun solvePartOne(): Int {
+    return readInput("/day4-input")
+        .map { it.split(",") }
+        .map { it[0].toRange() to it[1].toRange() }
+        .count { it.first.contains(it.second) || it.second.contains(it.first) }
+}
+
+fun solvePartTwo(): Int {
+    return readInput("/day4-input")
+        .map { it.split(",") }
+        .map { it[0].toRange() to it[1].toRange() }
+        .count { it.first.overlaps(it.second) || it.second.overlaps(it.first)}
+}
+
+fun String.toRange(): IntRange {
+    val rangeEnds = this.split("-")
+    return rangeEnds[0].toInt()..rangeEnds[1].toInt()
+}
+
+private fun IntRange.contains(other: IntRange): Boolean = this.contains(other.first) && this.contains(other.last)
+
+private fun IntRange.overlaps(other: IntRange): Boolean = this.contains(other.first) || this.contains(other.last)
